15c4726c55e762c0887d00086b8783967f1505a6
KyrylR <kyryl.ryabov@gmail.com>
2022-02-20 14:36:01 +0200

    Initial commit

---
+++ ChatRoom/server.py
@@ -0,0 +1,53 @@
+import threading
+import socket
+
+host = '172.105.104.152' #localhost
+port = 5555
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_ip = socket.gethostbyname(host)
+print(server_ip)
+server.bind((host, port))
+
+server.listen()
+
+clients = []
+nicknames = []
+
+def broadcast(message, n):
+    for client in clients:
+        if n != client:
+            client.send(message)
+
+def handle(client):
+    while True:
+        try:
+            message = client.recv(1024)
+            broadcast(message, client)
+        except:
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            nickname = nicknames[index]
+            broadcast(f'{nickname} left the chat'.encode('ascii'), client)
+            nicknames.remove(nickname)
+            break
+
+def receive():
+    while True:
+        client, address = server.accept()
+        print(f"Connect with {str(address)}")
+
+        client.send('NICK'.encode('ascii'))
+        nickname = client.recv(1024).decode('ascii')
+        nicknames.append(nickname)
+        clients.append(client)
+
+        print(f'Nickname of the client is {nickname}')
+        broadcast(f'{nickname} join the chat'.encode('ascii'), client)
+        client.send('Connected to the server!'.encode('ascii'))
+
+        thread = threading.Thread(target=handle, args=(client,))
+        thread.start()
+
+receive()
---
+++ Mail/main.py
@@ -0,0 +1,42 @@
+import smtplib
+from email import encoders
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+
+server = smtplib.SMTP('smtp.live.com', 587)
+server.connect("smtp.live.com", 587)
+server.ehlo()
+server.starttls()
+server.ehlo()
+
+with open('password.txt', 'r') as f:
+    password = f.read()
+
+server.login('test4edupc@hotmail.com', password)
+
+
+msg = MIMEMultipart()
+msg['From'] = 'Myself'
+msg['To'] = 'for.services.it.t@gmail.com'
+msg['Subject'] = 'Just a test23'
+
+with open('msg.txt', 'r') as f:
+    massage = f.read()
+
+msg.attach(MIMEText(massage, 'plain'))
+
+filename = 'default.jpg'
+attachment = open(filename, 'rb')
+
+p = MIMEBase('application', 'octet-stream')
+p.set_payload(attachment.read())
+
+encoders.encode_base64(p)
+p.add_header('Content-Disposition', f'attachment; filename={filename}')
+msg.attach(p)
+
+text = msg.as_string()
+
+server.sendmail('test4edupc@hotmail.com', 'for.services.it.t@gmail.com', text)
+server.quit()
